e31a24e1b0b8016c9a744b1b4330b4c1b4df4822
Anish <anish@example.com>
2025-11-21 00:30:15 -0500

    chore: clean up splash screen imports for analyzer

--- connexus_app/lib/presentation/screens/splash/splash_screen.dart
+++ connexus_app/lib/presentation/screens/splash/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../base_screen.dart';
-import '../../../core/routes/app_router.dart';
 import '../../../core/utils/logger.dart';
 
 class SplashScreen extends BaseScreen {
